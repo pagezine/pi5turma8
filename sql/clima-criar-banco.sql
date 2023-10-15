fa59da7103af5ea6979065016611c88a7e332382
Anderson PAGEZINE <anderson@pagezine.com.br>
2023-10-15 19:32:28 -0300

    Add files via upload

---
+++ sql/clima-criar-banco.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE clima;
+
+USE clima;
+
+CREATE TABLE leituras_clima (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT,
+    id_estacao INT,
+    estacao_lat DECIMAL(9, 6),
+    estacao_lon DECIMAL(9, 6),
+    gps_lat DECIMAL(9, 6),
+    gps_lon DECIMAL(9, 6),
+    data DATE,
+    hora TIME,
+    temperatura DECIMAL(5, 2),
+    umidade DECIMAL(5, 2),
+    dir_vento VARCHAR(255),
+    vel_vento DECIMAL(5, 2),
+    solar_uv DECIMAL(5, 2)
+);
